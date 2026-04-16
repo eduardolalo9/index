03710cf2bf88241e5fce27e020788f1a794b761d
eduardolalo9 <eduardolalo9606@gmail.com>
2026-04-15 20:29:53 -0600

    Rename FIREBASE_CONFIG to firebaseConfig

--- firebase-config.js
+++ firebase-config.js
@@ -30,15 +30,15 @@ window._firebaseReady = false;
 (function initFirebase() {
     'use strict';
 
-    const FIREBASE_CONFIG = {
-        apiKey:            "AIzaSyDugu23uEgacqMUTsoBF8i7xfyDIDbiv0M",
-        authDomain:        "bar-inventario-1109e.firebaseapp.com",
-        databaseURL:       "https://bar-inventario-1109e-default-rtdb.firebaseio.com",
-        projectId:         "bar-inventario-1109e",
-        storageBucket:     "bar-inventario-1109e.firebasestorage.app",
-        messagingSenderId: "450765028668",
-        appId:             "1:450765028668:web:54fdb19714d374ff02b239"
-    };
+const firebaseConfig = {
+  apiKey: "AIzaSyDugu23uEgacqMUTsoBF8i7xfyDIDbiv0M",
+  authDomain: "bar-inventario-1109e.firebaseapp.com",
+  databaseURL: "https://bar-inventario-1109e-default-rtdb.firebaseio.com",
+  projectId: "bar-inventario-1109e",
+  storageBucket: "bar-inventario-1109e.firebasestorage.app",
+  messagingSenderId: "450765028668",
+  appId: "1:450765028668:web:54fdb19714d374ff02b239"
+};
 
     // Verificar que la config no tiene valores de placeholder
     const configured = Object.values(FIREBASE_CONFIG).every(
